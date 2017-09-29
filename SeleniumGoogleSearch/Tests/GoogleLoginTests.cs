05e0907552c301cb4e53e06ba6b9490d519a1c70
Oleg <Oleg.Zarevych@gmail.com>
2017-09-30 00:31:04 +0300

    Fix sign up test

---
+++ SeleniumGoogleSearch/Tests/GoogleLoginTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using SeleniumGoogleSearch.Core;
+using SeleniumGoogleSearch.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumGoogleSearch.Tests
+{
+    [TestFixture]
+    public class GoogleLoginTests : TestBase
+    {
+        [Test]
+        public void OpenSignUpPage()
+        {
+            GoogleLoginPage login = Page.GoogleMain.OpenLoginPage();
+            GoogleRegisterPage signUp = login.MoreAndSignUp();
+            
+            ///TODO - problems with clicking
+        }
+    }
+}
